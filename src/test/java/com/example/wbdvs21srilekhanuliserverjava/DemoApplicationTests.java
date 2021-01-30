e9bfffd76bb8c9c43f7831a5cffc9dac1c5fa407
Srilekha Nuli <srilekhanuli@gmail.com>
2021-01-30 05:50:30 -0500

    initial commit

---
+++ src/test/java/com/example/wbdvs21srilekhanuliserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvs21srilekhanuliserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
