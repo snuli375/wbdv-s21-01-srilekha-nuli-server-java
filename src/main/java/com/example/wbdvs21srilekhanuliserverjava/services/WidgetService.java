47cbffb5416eefea17b62df0ae0dae15dd438bb9
Srilekha Nuli <srilekhanuli@gmail.com>
2021-04-26 08:20:10 -0400

    use repository

--- src/main/java/com/example/wbdvs21srilekhanuliserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvs21srilekhanuliserverjava/services/WidgetService.java
@@ -1,64 +1,48 @@
 package com.example.wbdvs21srilekhanuliserverjava.services;
 
 import com.example.wbdvs21srilekhanuliserverjava.models.Widget;
+import com.example.wbdvs21srilekhanuliserverjava.repositories.WidgetRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class WidgetService {
-  private final List<Widget> widgets = new ArrayList<Widget>();
+  @Autowired
+  WidgetRepository repository;
 
   public Widget createWidget(String tid, Widget widget) {
-    Long id = (new Date()).getTime();
-    widget.setId(id);
     widget.setTopicId(tid);
-    widgets.add(widget);
-    return widget;
+    return repository.save(widget);
   }
 
   public List<Widget> findWidgetsForTopic(String tid) {
-    List<Widget> filtered = new ArrayList<>();
-    for (Widget w : widgets) {
-      if (w.getTopicId().equals(tid)) {
-        filtered.add(w);
-      }
-    }
-    return filtered;
+    return repository.findWidgetsForTopic(tid);
   }
 
   public int updateWidget(Long wid, Widget widget) {
-    for (int i = 0; i < widgets.size(); i++) {
-      if (widgets.get(i).getId().equals(wid)) {
-        widgets.set(i, widget);
-      }
-    }
-    return -1;
+    Widget originalWidget = findWidgetById(wid);
+
+    originalWidget.setText(widget.getText());
+    originalWidget.setSrc(widget.getSrc());
+
+    repository.save(originalWidget);
+    return 1;
   }
 
   public int deleteWidget(Long wid) {
-    for (Widget w : widgets) {
-      if (w.getId().equals(wid)) {
-        widgets.remove(w);
-      }
-    }
-    return -1;
+    repository.deleteById(wid);
+    return 1;
   }
 
   public List<Widget> findAllWidgets() {
-    return widgets;
+    return repository.findAllWidgets();
   }
 
   public Widget findWidgetById(Long wid) {
-    for (Widget w : widgets) {
-      if (w.getId().equals(wid)) {
-        return w;
-      }
-    }
-    return null;
+    return repository.findWidgetById(wid);
   }
 
 
